c014b214b8d71364398fa0356b2647248b7c8052
kennyhub2001 <kennyosoro44@gmail.com>
2024-03-20 19:19:54 +0300

    created uperlinks for web pages

--- src/app.js
+++ src/app.js
@@ -1,9 +1,4 @@
-let menu=document.querySelector(".menu");
 let submenu=document.querySelector(".second-nav");
-
-function showMenu(){
-    menu.style.display = 'none' ? menu.style.display = "block" : menu.style.display = "none";
-}
 
 const showSubMenu = () => {
     submenu.style.top = '-150px' ? submenu.style.top = "50px" : submenu.style.top = "-150px";
